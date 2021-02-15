ded62f5f6fd8ca019e6d7ccc109fef3b33a514b2
Vishal643 <vishalkumar199812@gmail.com>
2021-02-15 08:30:25 +0530

    done merit_list

--- Unit - 2/week-4/day-1/merit_list.js
+++ Unit - 2/week-4/day-1/merit_list.js
@@ -1,11 +1,49 @@
 function runProgram(input) {
 	input = input.trim().split('\n');
 	let num = +input.shift();
+
+	const customSort = (a, b) => {
+		a[1] = Number(a[1]);
+		a[2] = Number(a[2]);
+		a[3] = Number(a[3]);
+
+		b[1] = Number(b[1]);
+		b[2] = Number(b[2]);
+		b[3] = Number(b[3]);
+
+		if (b[3] > a[3]) {
+			return 1;
+		} else if (b[3] < a[3]) {
+			return -1;
+		} else if (b[3] === a[3]) {
+			if (b[1] > a[1]) {
+				return 1;
+			} else if (b[1] < a[1]) {
+				return -1;
+			} else if (b[1] === a[1]) {
+				if (b[2] < a[2]) {
+					return 1;
+				} else if (b[2] > a[2]) {
+					return -1;
+				} else if (b[2] === a[2]) {
+					if (b[0] < a[0]) {
+						return 1;
+					} else {
+						return -1;
+					}
+				}
+			}
+		}
+	};
+
 	let a = input
 		.map((el) => el.trim().split(' '))
-		.map((el) => [el[0], +el[1], +el[2], +el[3]]);
-	
-	
+		.map((el) => [el[0], +el[1], +el[2], +el[3]])
+		.sort(customSort);
+
+	for (let i = 0; i < 8; i++) {
+		console.log(a[i][0]);
+	}
 }
 
 if (process.env.USERNAME === 'vishal') {
